32c19508f30854926ab737f4f7d177e179fff262
JacobFrederic <115405752+JacobFrederic@users.noreply.github.com>
2022-11-18 16:15:07 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -1,7 +1,7 @@
-#First exploration of working with landsat data at 20m spatial resolution from the amazon forest 
+#First exploration of working with landsat data at 30m spatial resolution from the amazon forest 
 #The image's name is p224r63_2011: path 224 and row 63 from 2011
 library(raster)
-
+setwd("C:/Users/jacob/Documents/R/lab")
 #To upload the image the function ~brick("") can be used which creates a rasterbrick object containing several bands of one satellite image, formed by a pixelmatrix
 p224r63_2011 <- brick("p224r63_2011_masked.grd") #In this case we assign the function to the image name
 #Next a plot of the image can be created
@@ -15,26 +15,23 @@ plot(p224r63_2011,col=colour)                   #plot with the specified colours
 #mfrowspecifies the size with the depth and width
 par(mfrow=c(2,2))
 
-#To dispaly the different bands in different colors multiple colour palettes can be created and each be plotted
+#To display the different bands in different colors multiple colour palettes can be created and each be plotted
 
 #band 1 in blue
-clb <- colorRampPalette(c("blue4", "royalblue2", "skyblue"))(100) 
-plot(p224r63_2011$B1_sre,col=clb) # band 1 = B1_sre
+clb <- colorRampPalette(c("blue4","royalblue2","skyblue"))(100) 
+plot(p224r63_2011$B1_sre,col=clb)               #band 1 = B1_sre
+#band 2 in green
+clg <- colorRampPalette(c("dark green","palegreen4","darkseagreen1"))(100) 
+plot(p224r63_2011$B2_sre, col=clg)              #band 2 = B2_sre
+#band 3 in red
+clr <- colorRampPalette(c("brown4","red3","indianred1"))(100) 
+plot(p224r63_2011$B3_sre, col=clr)              #band 3 = B3_sre
+#band 4, the NearInfraRed
+clnir <- colorRampPalette(c("firebrick4","darkorange2","lightgoldenrod"))(100) 
+plot(p224r63_2011$B4_sre, col=clnir)            #band 4 = B4_sre 
 
-# repeat for band 2 in green
-clg <- colorRampPalette(c("dark green", "palegreen4", "darkseagreen1"))(100) 
-plot(p224r63_2011$B2_sre, col=clg) # band 2 = B2_sre
-
-# repeat for band 3 in red
-clr <- colorRampPalette(c("brown4", "red3", "indianred1"))(100) 
-plot(p224r63_2011$B3_sre, col=clr) # band 3 = B3_sre
-
-# plot the final band, the NearInfraRed, which is band number 4 using a new color palette
-clnir <- colorRampPalette(c("firebrick4", "darkorange2", "lightgoldenrod"))(100) 
-plot(p224r63_2011$B4_sre, col=clnir) # band 4 = B4_sre 
-
-#The function ~plotRGB() can be used t0 stack the different band layers on top of each other
-plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
-plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
-plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
-plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+#The function ~plotRGB() can be used to stack the different band layers on top of each other
+plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin")
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
+plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="Lin")
+plotRGB(p224r63_2011,r=3,g=2,b=4,stretch="Lin")
