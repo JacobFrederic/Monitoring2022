d347521f8159711ed11bc871a141c42da86426f2
JacobFrederic <115405752+JacobFrederic@users.noreply.github.com>
2022-11-18 17:03:22 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -1,5 +1,7 @@
 #First exploration of working with landsat data at 30m spatial resolution from the amazon forest 
 #The image's name is p224r63_2011: path 224 and row 63 from 2011
+install.packages("RStoolbox")
+library(RStoolbox)
 library(raster)
 setwd("C:/Users/jacob/Documents/R/lab")
 #To upload the image the function ~brick("") can be used which creates a rasterbrick object containing several bands of one satellite image, formed by a pixelmatrix
@@ -22,16 +24,33 @@ clb <- colorRampPalette(c("blue4","royalblue2","skyblue"))(100)
 plot(p224r63_2011$B1_sre,col=clb)               #band 1 = B1_sre
 #band 2 in green
 clg <- colorRampPalette(c("dark green","palegreen4","darkseagreen1"))(100) 
-plot(p224r63_2011$B2_sre, col=clg)              #band 2 = B2_sre
+plot(p224r63_2011$B2_sre,col=clg)               #band 2 = B2_sre
 #band 3 in red
 clr <- colorRampPalette(c("brown4","red3","indianred1"))(100) 
-plot(p224r63_2011$B3_sre, col=clr)              #band 3 = B3_sre
+plot(p224r63_2011$B3_sre,col=clr)               #band 3 = B3_sre
 #band 4, the NearInfraRed
 clnir <- colorRampPalette(c("firebrick4","darkorange2","lightgoldenrod"))(100) 
-plot(p224r63_2011$B4_sre, col=clnir)            #band 4 = B4_sre 
+plot(p224r63_2011$B4_sre,col=clnir)             #band 4 = B4_sre 
 
 #The function ~plotRGB() can be used to stack the different band layers on top of each other
 plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin")
 plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
 plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="Lin")
 plotRGB(p224r63_2011,r=3,g=2,b=4,stretch="Lin")
+dev.off() # close the multiframe
+
+#histogram stretching
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="hist") #this stretches the values it's possiblt to look inside the forest better (for example seeing bare soil and openings within the forest)
+
+#To see the changes over time in the same place we can upload another image from 1988
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+#Use the ~plotRGB() function and assign the parameters to the different bands according to the following to create a plot in natural colours
+plotRGB(p224r63_1988,r=3,g=2,b=1,stretch="lin")
+#b4 contains the NearInfraRed data, by adding g=4 instead of g=2 (and b=2), the data of it is visualized as green in the image, showing the distribution of area with forest
+plotRGB(p224r63_1988,r=3,g=4,b=2,stretch="lin")
+#To show the difference between 1988 and 2011 the following set of commands can be followed to show the two corresponding plots next to each other
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988,r=3,g=4,b=2,stretch="lin")
+plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="lin")
