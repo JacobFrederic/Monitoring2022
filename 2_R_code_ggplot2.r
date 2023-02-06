c3c8d5bde0dfb095cccd920848acbeaa4fb62113
JacobFrederic <115405752+JacobFrederic@users.noreply.github.com>
2023-02-06 13:44:05 +0100

    Update and rename R_code_ggplot2.r to 2_R_code_ggplot2.r

--- 2_R_code_ggplot2.r
+++ 2_R_code_ggplot2.r
@@ -1,12 +1,18 @@
+#Introduction to ggplots
+
 install.packages("ggplot2")             #installs the required package
 library(ggplot2)                        #function to open the  package
+
 virus <- c(10,30,40,50,60,80)           #example for measurements of the occurence of a virus as an array
 death <- c(100, 240, 310, 470, 580,690) #accordingly, a number of corresponding death measurements
+
 plot (virus,death)                      #old way to create a plot
+
 #Alternatively you can create a data frame that will assign the variables to a corresponding table
 data.frame(virus,death)
 d <- data.frame(virus,death)            #assigns the data frame to an object d
 summary(d)                              #overview of the object and shows univariate statistical info
+
 ggplot(d,aes(x=virus,y=death)) + geom_point()      
 #requires to define the variables as asthetics, so geom_point will find them. 
 #geom_points creates a scatterplot with the data
@@ -21,5 +27,6 @@ read.table("covid_agg.csv",header=TRUE)
 #header=TRUE describes that the first row in the table is not a set of data but contains the names of the variables
 covid <- read.table("covid_agg.csv",header=TRUE) #assigns the table to the object "covid"
 summary(covid)                          #overview that also includes the information of maximum, minimum, mean and average of cases per country
+
 #Now the ~ggplot() function can be used to create an image of the spatial distribution with varying point sizes to illustrate the amount of cases per country
 ggplot(covid,aes(x=lon,y=lat,size=cases)) + geom_point()
