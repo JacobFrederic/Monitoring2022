43c5bcfdff982fee03c7ad5db12981fbb1aa2c3d
JacobFrederic <115405752+JacobFrederic@users.noreply.github.com>
2023-02-06 13:48:58 +0100

    Update and rename R_code_spectral_indices.r to R_code_Spectral_Indices.r

--- R_code_Spectral_Indices.r
+++ R_code_Spectral_Indices.r
@@ -5,6 +5,7 @@ library(ggplot2)    #for the final histogram plot
 library(patchwork)  #to show the histogram plots together
 library(gridExtra)  #to show the histogram plots together
 setwd("C:/Users/jacob/Documents/R/lab")
+
 l1992 <- brick("defor1.png")
 #Bands contain the following information: 1 is NIR, 2 red and 3 green
 #Plot the png data with NIR being shown as red, red as green and green as blue
@@ -12,10 +13,12 @@ plotRGB(l1992,r=1,g=2,b=3,stretch="Lin")
 #Proceed accordingly with the picture from 2006
 l2006 <- brick("defor2.png")
 plotRGB(l2006,r=1,g=2,b=3,stretch="Lin")
+
 #Using the ~par() function we can see the difference of the location between 1992 and 2006
 par(mfrow=c(2,1))
 plotRGB(l1992,r=1,g=2,b=3,stretch="Lin")
 plotRGB(l2006,r=1,g=2,b=3,stretch="Lin")
+
 #Calculating the vegetation indices using the DVI 
 #DVI is a unit that puts NIR and red in relation to estimate the vegeation coverage
 #A small difference relates to low plant coverage or plants in a stressed state
@@ -26,6 +29,7 @@ dvi2006 <- l2006[[1]]-l2006[[2]]
 plot(dvi2006,col=cl)
 #The resulting images highlight a significant loss of forested (red) area in favour of more agricultural land use (yellow)
 #Using R the areas can be classified into different categories using an automatically generated values of the NIR and red values
+
 #In the following a unsupervised classification will be conducted
 d1c <- unsuperClass(l1992,nClasses=2) #nClasses=2 signals that there will be 2 classes
 d1c             #showing some of the characteristics of d1c
@@ -38,6 +42,7 @@ h1992
 # 1992
 # class 1: forest - 306841 - ~90 %
 # class 2: human  -  34451 - ~10 %
+
 #Proceeding accordingly with the data from 2006
 d2c <- unsuperClass(l2006,nClasses=2)
 d2c
